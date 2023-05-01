8643cf3248bbdf059a1436a4ff57e7aaf0fbcb2a
joshua <jbroeksteeg0@gmail.com>
2023-05-02 07:57:40 +1000

    most of cursor working, edgecases still exist

--- src/backend/BufferCursor.cpp
+++ src/backend/BufferCursor.cpp
@@ -1,8 +1,8 @@
 #include "BufferCursor.h"
 #include "Buffer.h"
 
-BufferCursor::BufferCursor(Buffer *buffer) : m_buffer(buffer), m_index(0) {
-}
+BufferCursor::BufferCursor(Buffer *buffer)
+    : m_buffer(buffer), m_index(0) {}
 
 void BufferCursor::delete_character_before() {
   if (m_index == 0)
@@ -35,75 +35,78 @@ void BufferCursor::move_right() {
 }
 
 void BufferCursor::move_up() {
-  // calculate char's position on line
-  int pos_on_line = 0;
-  int temp_index = m_index;
-  while (temp_index > 0 && m_buffer->m_rope.at(temp_index-1) != '\n') {
-    temp_index--;
-    pos_on_line++;
-  }
+  int cursor_pos = m_index;
 
-  // find index of pos_on_line but on next line
-  // if there is no new line, keep as -1 and do nothing
-  int start_of_prev_line = -1;
-  int newlines_found = 0; // number of newlines or the beginning of the line
+  // -2 default value, not set
+  int prev_newline = -2, second_prev_newline = -2;
 
-  temp_index = m_index;
-  while (temp_index > 0) {
-    temp_index--;
-    if (temp_index == 0 || m_buffer->m_rope.at(temp_index) == '\n')
-      newlines_found++;
+  int temp_index = m_index - 1;
 
-    // has found the beginning of the prev line
-    if (newlines_found == 2) {
-      start_of_prev_line = temp_index;
-      break;
+  while (second_prev_newline == -2 && temp_index >= 0
+  ) {    // TODO: optimise out m_rope.at
+    char current_char = m_buffer->m_rope.at(temp_index);
+    if (temp_index == 0 && current_char == '\n' && prev_newline == -2) {
+      prev_newline = 0;
+      second_prev_newline = -1;
+      std::cout << " in here" << std::endl;
+    } else if (current_char == '\n' && prev_newline == -2) {
+      prev_newline = temp_index;
+    } else if (current_char == '\n' && second_prev_newline == -2) {    // if there's a normal newline behind
+      second_prev_newline = temp_index;
+    } else if (temp_index == 0 && prev_newline != -2) {    // can also set the start of the prev line to index -1
+      second_prev_newline = -1;
     }
+    temp_index--;
   }
 
+  // if there is a line above, move onto it
+  if (second_prev_newline != -2) {
+    int new_pos = second_prev_newline + 1;
+    int dist = cursor_pos - prev_newline - 1;
 
-  // if there is a prev line move cursor
-  if (newlines_found == 2) {
-    temp_index = start_of_prev_line;
-    // while the cursor can and should be moved forward
-    while (m_buffer->m_rope.at(temp_index) != '\n' && temp_index + 1 < (int)m_buffer->m_rope.size() && (temp_index - start_of_prev_line) < pos_on_line)
-      temp_index++;
-
-    m_index = temp_index;
+    while (dist > 0 && (new_pos <= m_buffer->m_rope.size() || m_buffer->m_rope.at(new_pos) != '\n')) {
+      new_pos++;
+      dist--;
+    }
+    m_index = new_pos;
   }
 }
 
 void BufferCursor::move_down() {
   // calculate char's position on line
-  int pos_on_line = 0;
-  int temp_index = m_index;
-  while (temp_index > 0 && m_buffer->m_rope.at(temp_index-1) != '\n') {
-    temp_index--;
+  int pos_on_line = 1;
+  int temp_pos = m_index - 1;
+  while (temp_pos >= 0
+         && m_buffer->m_rope.at(temp_pos) != '\n') {
+    temp_pos--;
     pos_on_line++;
   }
 
-  // go to the end of the line
-  temp_index = m_index;
-  int beginning_of_next_line = -1;
-  while (temp_index+1 < (int)m_buffer->m_rope.size()) {
-    temp_index++;
-    if (m_buffer->m_rope.at(temp_index) == '\n') {
-      beginning_of_next_line = temp_index;
-      break;
-    }
+  // the next newline
+  int start_of_next = m_index;
+  while (start_of_next + 1 < m_buffer->m_rope.size()
+         && m_buffer->m_rope.at(start_of_next) != '\n')
+    start_of_next++;
+
+  // if there is no newline, return
+  if (start_of_next == m_buffer->m_rope.size() || m_buffer->m_rope.at(start_of_next) != '\n') {
+    return;
   }
 
-  // if there is a new line, place as far as possible
-  if (beginning_of_next_line != -1) {
-    temp_index = beginning_of_next_line;
-    // while the cursor can and should be moved forward
-    while (m_buffer->m_rope.at(temp_index) != '\n' && temp_index + 1 < (int)m_buffer->m_rope.size() && (temp_index - beginning_of_next_line) < pos_on_line)
-      temp_index++;
+  int new_pos =
+    start_of_next + 1;    // handle newline edge with +1
+  pos_on_line--;
 
-    m_index = temp_index;
+  while (
+    pos_on_line > 0
+    && new_pos < m_buffer->m_rope.size()
+    && (new_pos+1==m_buffer->m_rope.size() || m_buffer->m_rope.at(new_pos) != '\n')
+  ) {
+    pos_on_line--;
+    new_pos++;
   }
+
+  m_index = new_pos;
 }
 
-int BufferCursor::get_index() const {
-  return m_index;
-}
+int BufferCursor::get_index() const { return m_index; }
